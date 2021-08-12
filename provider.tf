c4f13c0591fa6fdf523d90ef5589dd8a29928787
Roman Simecek <raskhadafi@users.noreply.github.com>
2021-08-12 13:45:29 +0200

    Setup k8s provider.

--- provider.tf
+++ provider.tf
@@ -8,3 +8,10 @@ provider "helm" {
 
   }
 }
+
+provider "kubernetes" {
+  host = var.rancher_k8s.host
+  client_certificate     = var.rancher_k8s.client_certificate
+  client_key             = var.rancher_k8s.client_key
+  cluster_ca_certificate = var.rancher_k8s.cluster_ca_certificate
+}
